d763fbe4d66a051ffc79df18725763d28b03ab7c
mrcomputer2018 <45561350+mrcomputer2018@users.noreply.github.com>
2021-10-11 16:50:08 -0300

    criacao do menu principal

--- src/application/Menu.java
+++ src/application/Menu.java
@@ -10,6 +10,23 @@ public class Menu {
     // Metodos
     public void iniciar() {
         String opcao = mostrarMenu();
+
+        while(!opcao.equals("4")){
+            switch (opcao) {
+                case "1":
+                    opcao = mostrarMenuReservas();
+                    break;
+                case "2":
+                    opcao = mostrarMenuHospedes();
+                    break;
+                case "3":
+                    opcao = mostrarMenuQuartos();
+                    break;
+                default:
+                    System.out.println("Opcao invalida!!!");
+                    opcao = mostrarMenu();
+            }
+        }
         System.out.println("Inicio");
     }
 
@@ -31,20 +48,56 @@ public class Menu {
         return entradaCaracter.nextLine();
     }
 
-    private String mostrarMenuReserva() {
+    private String mostrarMenuReservas() {
         StringBuilder sb = new StringBuilder();
 
         sb.append("===================\n");
         sb.append("       RESERVAS      \n");
         sb.append("===================\n");
         sb.append("Digite comando desejado: \n");
-        sb.append("\t1. Ver lista. \n");
-        sb.append("\t2. Adicionar item a lista.  \n");
-        sb.append("\t3. Remover item da lista. \n");
-        sb.append("\t4. Limpar lista. \n");
-        sb.append("\t5. Sair de Reserva.");
+        sb.append("\t1. Ver reservas. \n");
+        sb.append("\t2. Adicionar item a reservas.  \n");
+        sb.append("\t3. Remover item da reservas. \n");
+        sb.append("\t4. Limpar reservas. \n");
+        sb.append("\t5. Sair de Reservas.");
 
         // Convertendo a classe stringbuilder para string
+        System.out.println(sb.toString());
+
+        return entradaCaracter.nextLine();
+    }
+
+    private String mostrarMenuHospedes() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("===================\n");
+        sb.append("      HOSPEDES     \n");
+        sb.append("===================\n");
+        sb.append("Digite comando desejado: \n");
+        sb.append("\t1. Ver hospedes. \n");
+        sb.append("\t2. Adicionar item a hospedes.  \n");
+        sb.append("\t3. Remover item de hospedes. \n");
+        sb.append("\t4. Limpar hospedes. \n");
+        sb.append("\t5. Sair de hospedes.");
+
+        System.out.println(sb.toString());
+
+        return entradaCaracter.nextLine();
+    }
+
+    private String mostrarMenuQuartos() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("===================\n");
+        sb.append("       QUART0S     \n");
+        sb.append("===================\n");
+        sb.append("Digite comando desejado: \n");
+        sb.append("\t1. Ver quartos. \n");
+        sb.append("\t2. Adicionar item a quartos.  \n");
+        sb.append("\t3. Remover item de quartos. \n");
+        sb.append("\t4. Limpar quartos. \n");
+        sb.append("\t5. Sair de quartos.");
+
         System.out.println(sb.toString());
 
         return entradaCaracter.nextLine();
