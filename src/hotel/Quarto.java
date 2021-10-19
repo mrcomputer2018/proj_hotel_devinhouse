068cc3277b9703331a2c5b975dc23f2ee35848d0
mrcomputer2018 <45561350+mrcomputer2018@users.noreply.github.com>
2021-10-19 08:33:23 -0300

    atributos classe Quarto

--- src/hotel/Quarto.java
+++ src/hotel/Quarto.java
@@ -1,4 +1,9 @@
 package hotel;
 
 public class Quarto {
+    //Atributos
+    private TipoQuarto tipoQuarto;
+    // 90/140 baixa - 150/400 alta
+    private static final double precoBaseQuartoSimples = 90.00;
+    private static final double precoBaseQuartoLuxo = 140.00;
 }
