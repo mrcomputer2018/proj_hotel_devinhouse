5c39f3c4ff8c0ec1bffa9658ecd85f451b4e5038
mrcomputer2018 <45561350+mrcomputer2018@users.noreply.github.com>
2021-10-19 08:55:08 -0300

    metodos classe Quarto

--- src/hotel/Quarto.java
+++ src/hotel/Quarto.java
@@ -6,4 +6,12 @@ public class Quarto {
     // 90/140 baixa - 150/400 alta
     private static final double precoBaseQuartoSimples = 90.00;
     private static final double precoBaseQuartoLuxo = 140.00;
+
+    //Metodos
+    public Quarto(TipoQuarto tipoQuarto) {
+        this.tipoQuarto = tipoQuarto;
+    }
+    public TipoQuarto getTipoQuarto() {
+        return tipoQuarto;
+    }
 }
