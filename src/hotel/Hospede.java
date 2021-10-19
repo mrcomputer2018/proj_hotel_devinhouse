a07c2912fb600599d13e5599f8ec62c2f1215a0f
mrcomputer2018 <45561350+mrcomputer2018@users.noreply.github.com>
2021-10-19 15:41:25 -0300

    atributos Classe Hospede

--- src/hotel/Hospede.java
+++ src/hotel/Hospede.java
@@ -3,10 +3,18 @@ package hotel;
 import java.time.LocalDate;
 
 public abstract class Hospede {
-
+    // Atributos
     private String nome;
     private String telefone;
     private TipoDocumento tipoDocumento;
     private LocalDate dataDeNascimento;
     private TipoHospede tipoHospede;
+
+    //Getters e Setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getNome() {
+        return nome;
+    }
 }
